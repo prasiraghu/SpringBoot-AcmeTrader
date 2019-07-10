d706d0892eaa09c39be8d80855ad98681c949529
vlad olenin <volenin@gmail.com>
2019-07-10 13:18:18 -0500

    h2 setup

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.DAO.h2Setup;
+
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		h2Setup test = new h2Setup();
+		test.insertRecordInTableUsingStatement();
+		
 	}
 
 }
