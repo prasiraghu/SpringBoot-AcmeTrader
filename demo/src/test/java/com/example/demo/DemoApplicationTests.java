f4a877c4832ca1d0c8bcc802a05a1b77cf51639c
vlad olenin <volenin@gmail.com>
2019-07-11 14:28:17 -0500

    Initial Tests

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,48 @@
 package com.example.demo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.matcher.RestAssuredMatchers.*;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.hamcrest.Matchers.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void EndpointTest() {
+		when().get("/trader/stockquotes").then().statusCode(200);
+	}
+
+	@Test
+	public void TraderAbleToViewStockQuotes() {
+		when().get("/trader/stockquotes").then().assertThat().body("stock_symbol", notNullValue());
+	}
+
+	@Test
+	public void TraderAddStockSuccessfully() {
+		when().post("/trader/stockquotes").then().statusCode(201);
+	}
+
+	@Test
+	public void TraderDeleteStockSuccessfully() {
+		when().delete("/trader/stockquotes").then().statusCode(200);
 	}
 
 }
