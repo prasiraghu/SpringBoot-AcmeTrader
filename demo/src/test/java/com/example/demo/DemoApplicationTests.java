acd56f6dfe16666669f457592f80f029be932f5c
vlad olenin <volenin@gmail.com>
2019-07-11 15:29:24 -0500

    Possible JUnit fix

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,25 +1,14 @@
 package com.example.demo;
 
-import org.junit.Before;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static io.restassured.RestAssured.when;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.when;
-import static io.restassured.RestAssured.*;
-
-import io.restassured.matcher.RestAssuredMatchers.*;
-
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -37,12 +26,14 @@ public class DemoApplicationTests {
 
 	@Test
 	public void TraderAddStockSuccessfully() {
-		when().post("/trader/stockquotes").then().statusCode(201);
+		when().post("/trader/stockquotes").then().statusCode(200);
 	}
 
 	@Test
 	public void TraderDeleteStockSuccessfully() {
 		when().delete("/trader/stockquotes").then().statusCode(200);
 	}
+	
+	
 
 }
