cf04567348b0ad87097670cefb866b0c8ab9001e
vlad olenin <volenin@gmail.com>
2019-07-12 14:30:07 -0500

    More tests

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,48 +1,57 @@
 package com.example.demo;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertEquals;
-
-import java.awt.PageAttributes.MediaType;
-
-import org.apache.http.client.methods.RequestBuilder;
-
 import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.omg.CORBA.PRIVATE_MEMBER;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.demo.admin.controller.AdminController;
+import com.example.demo.admin.domain.AdminAccountVO;
 import com.example.demo.trader.controller.TraderController;
 import com.example.demo.trader.domain.TraderVO;
 import com.example.demo.trader.service.TraderService;
-import com.example.demo.trader.service.TraderServiceImpl;
-
+import com.example.demo.utils.TestUtil;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest
 public class DemoApplicationTests {
-
+	@Autowired
+	private TraderController traderController;
+	@Autowired
+	private AdminController adminController;
+	@Autowired
+	private MockMvc mockMvc;
 //	@Test
-//	public void EndpointTest() {
+//	public void EndpointTest1() {
 //		when().get("/trader/stockquotes").then().statusCode(200);
 //	}
 //
@@ -50,20 +59,109 @@ public class DemoApplicationTests {
 //	public void TraderAbleToViewStockQuotes() {
 //		when().get("/trader/stockquotes").then().assertThat().body("stock_symbol", notNullValue());
 //	}
-//
-//	@Test
-//	public void TraderAddStockSuccessfully() {
-//		when().post("/trader/stockquotes").then().statusCode(200);
-//	}
-//
-//	@Test
-//	public void TraderDeleteStockSuccessfully() {
-//		when().delete("/trader/stockquotes").then().statusCode(200);
-//	}
+
+	@Test
+	public void StockQuoteHasAllMinimalInfo() {
+		// init
+		TraderVO stockQuote1 = new TraderVO();
+
+		// condition
+		stockQuote1.setStock_symbol("AAPL");
+		stockQuote1.setMarket_date("2018-01-24");
+		stockQuote1.setOpen((float) 160.36);
+		stockQuote1.setHigh((float) 160.63);
+		stockQuote1.setLow((float) 158.4);
+		stockQuote1.setVolume(20340900);
+
+		// validation
+		assertNotNull(stockQuote1);
+	}
+
+	@Test
+	public void AddStockQuote() throws Exception {
+		// init
+		TraderVO stockQuote2 = new TraderVO();
+		stockQuote2.setStock_symbol("TEST1");
+		stockQuote2.setMarket_date("2018-01-23");
+		stockQuote2.setOpen((float) 170.36);
+		stockQuote2.setHigh((float) 170.63);
+		stockQuote2.setLow((float) 168.4);
+		stockQuote2.setVolume(20340900);
+
+		// condition
+		ResponseEntity<TraderVO> responseEntity = (ResponseEntity<TraderVO>) traderController
+				.submitTickerAndMarketDate(stockQuote2);
+
+		// validation
+		assertEquals(HttpStatus.CREATED.value(), responseEntity.getStatusCodeValue());
+		System.out.println(HttpStatus.CREATED.value());
+		System.out.println(responseEntity.getStatusCodeValue());
+	}
+
+	@Test
+	public void DeleteStockQuote() throws Exception {
+		// init
+		TraderVO stockQuote2 = new TraderVO();
+		stockQuote2.setStock_symbol("TEST1");
+		stockQuote2.setMarket_date("2018-01-23");
+		stockQuote2.setOpen((float) 170.36);
+		stockQuote2.setHigh((float) 170.63);
+		stockQuote2.setLow((float) 168.4);
+		stockQuote2.setVolume(20340900);
+
+		// condition
+		ResponseEntity<TraderVO> responseEntity1 = (ResponseEntity<TraderVO>) traderController
+				.submitTickerAndMarketDate(stockQuote2);
+		// init
+		TraderVO stockQuote3 = new TraderVO();
+		stockQuote3.setStock_symbol("TEST1");
+		stockQuote3.setMarket_date("2018-01-23");
+
+		// condition
+		ResponseEntity<TraderVO> responseEntity2 = (ResponseEntity<TraderVO>) traderController
+				.deleteTickerAndMarketDate(stockQuote3);
+
+		// validation
+		assertEquals(HttpStatus.OK.value(), responseEntity2.getStatusCodeValue());
+		System.out.println(HttpStatus.OK.value());
+		System.out.println(responseEntity2.getStatusCodeValue());
+	}
 	
-	@Autowired
-	private MockMvc mockMvc;
-	
+	@Test
+	public void DeleteStockQuoteThatDoesNotExist() throws Exception {
+		// init
+		TraderVO stockQuote3 = new TraderVO();
+		stockQuote3.setStock_symbol("TEST");
+		stockQuote3.setMarket_date("2018-01-23");
+
+		// condition
+		ResponseEntity<TraderVO> responseEntity = (ResponseEntity<TraderVO>) traderController
+				.deleteTickerAndMarketDate(stockQuote3);
+
+		// validation
+		assertEquals(HttpStatus.NOT_FOUND.value(), responseEntity.getStatusCodeValue());
+		System.out.println(HttpStatus.NOT_FOUND.value());
+		System.out.println(responseEntity.getStatusCodeValue());
+	}
+//	@Test
+//	public void CreateAccount() throws Exception {
+//		// init
+//		AdminAccountVO account = new AdminAccountVO();
+//		account.setFirstName("John");
+//		account.setLastName("Doe");
+//		account.setEmail("abc@abc.com");
+//		account.setPhoneNumber("5555555555");
+////		account.setAccountNumber("AAA000001");
+////		account.setTraderId("");
+//
+//		// condition
+//		ResponseEntity<AdminAccountVO> responseEntity = (ResponseEntity<AdminAccountVO>) adminController
+//				.createAcct(account);
+//
+//		// validation
+//		assertEquals(HttpStatus.CREATED.value(), responseEntity.getStatusCodeValue());
+//		System.out.println(responseEntity.getStatusCodeValue());
+//	}
 	@Test
 	public void submitTickerAndMarketDateTest() throws Exception {
 		
@@ -89,7 +187,4 @@ public class DemoApplicationTests {
 		//confirm that successful entry is CREATED
 		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
 	}
-	
-	
-
 }
